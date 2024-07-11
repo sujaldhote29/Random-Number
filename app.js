67ee8658a193643f285c5728a059de63698d1e7b
Sujal <sujaldhote1@gmail.com>
2024-07-11 12:07:20 +0530

    new  changes added

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 let btn = document.querySelector("button");
+let skull = document.querySelector("body");
 
 btn.addEventListener("click",()=>{
-    let num = Math.floor(Math.random()*255+ 1);
-    let para = document.querySelector("p");
+    let num = Math.floor(Math.random()*255);
+    let para = document.getElementById("random");
     para.innerText = num;
 })
 
-
-
-
+btn.addEventListener("click",()=>{
+    let r = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    skull.style.backgroundColor = `rgb(${r},${g},${b})`;
+    
+})
